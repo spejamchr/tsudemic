81ca583eecbf7bfd532f26b65d202e04ed08d33a
Spencer Christiansen <jc.spencer92@gmail.com>
2020-04-12 00:12:56 -0600

    Dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import React from "react";
 import AppBody from "./components/AppBody";
 
+const theme = createMuiTheme({
+  palette: {
+    type: "dark",
+  },
+});
+
 const App = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <AppBody />
-    </>
+    </ThemeProvider>
   );
 };
 
