a608ec670cd10b31f4242de16e92ae38a4fcc6f8
Spencer Christiansen <jc.spencer92@gmail.com>
2020-04-13 12:11:45 -0600

    Fix the production build
    For some reason this comment broke production, but not development.
    Clicking the "Start" button gave an error:
    
    > Error: Minified React error #152; visit
    > https://reactjs.org/docs/error-decoder.html?invariant=152&args[]=X for
    > the full message or use the non-minified dev environment for full
    > errors and additional helpful warnings.
    
    From the link, the full text of the error is:
    
    > X(...): Nothing was returned from render. This usually means a return
    > statement is missing. Or, to render nothing, return null.
    
    When I originally put the comment there I remember thinking it was
    interesting that my linter/syntax checker didn't complain...

--- src/components/DrawPerson/PersonPath/SimplePath.tsx
+++ src/components/DrawPerson/PersonPath/SimplePath.tsx
@@ -7,8 +7,8 @@ interface Props {
   color: string;
 }
 
+// Add 50% alpha to the stroke color... I didn't find a better way to do this.
 const SimplePath: React.FunctionComponent<Props> = ({ x, y, r, color }) => (
-  // Add 50% alpha to the stroke color... I didn't find a better way to do this.
   <circle cx={x} cy={y} r={r} stroke={color + "7f"} strokeWidth="1" fill="none" />
 );
 
